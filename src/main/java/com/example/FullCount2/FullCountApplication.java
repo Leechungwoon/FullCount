4507629cbed007603ae2e57a4a506af1127e81da
Leechungwoon <dlcjddns22@gmail.com>
2026-04-15 14:49:03 +0900

    Initial commit

---
+++ src/main/java/com/example/FullCount2/FullCountApplication.java
@@ -0,0 +1,13 @@
+package com.example.FullCount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullCountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FullCount/FullCountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FullCount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullCountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
