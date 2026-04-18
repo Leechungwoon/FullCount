14e61eb92965cb8d1dea1c7f281b4a50ba9e69fb
Leechungwoon <dlcjddns22@gmail.com>
2026-04-18 17:30:21 +0900

    refactor: RequestMapping 변경 "/payment" -> "/payments"

--- src/main/java/com/example/FullCount2/domain/payment/controller/PaymentController.java
+++ src/main/java/com/example/FullCount2/domain/payment/controller/PaymentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/payment")
+@RequestMapping("/payments")
 public class PaymentController {
 
     private final PaymentService paymentService;
