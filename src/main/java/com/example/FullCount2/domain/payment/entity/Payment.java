8d00a36674e4ec2dd6bfc1d96db1aaba1bd7f3fa
Leechungwoon <dlcjddns22@gmail.com>
2026-04-18 17:32:09 +0900

    feat: Payment 엔티티 필드 추가
    - PaymentMethod Enum 추가
    - 결제 시간 paidAt 추가 -> PANDING시 null로 반환

--- src/main/java/com/example/FullCount2/domain/payment/entity/Payment.java
+++ src/main/java/com/example/FullCount2/domain/payment/entity/Payment.java
@@ -1,12 +1,16 @@
 package com.example.FullCount2.domain.payment.entity;
 
+import com.example.FullCount2.common.enums.PaymentMethod;
 import com.example.FullCount2.common.global.BaseEntity;
 import com.example.FullCount2.domain.reservation.entity.Reservation;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "payments")
@@ -30,6 +34,13 @@ public class Payment extends BaseEntity {
 
     @Column(nullable = false, length = 20)
     private String status = "PENDING"; // PENDING / DONE / CANCELLED
+
+    @Column(nullable = false, length = 30)
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod method = PaymentMethod.BANK_TRANSFER; //무통장 입금
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private LocalDateTime paidAt;
 
     @Builder
     private Payment(Reservation reservation, int amount) {
